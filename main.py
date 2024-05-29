5a8482c71eaeb7e3a531e76b8bd63711df3dd00c
Alexander Goussas <goussasalexander@gmail.com>
2024-05-29 13:36:07 -0500

    Fix flake8 errors

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 class myclass:
     def __init__(self):
         self.myFav = {'Paris': 500, 'NYC': 600}
-    
+
     def get_extraCost(self, dist):
         return self.myFav.get(dist, 0)
-    
-    def validThis(self, dist):
-        return type(dist) == str
 
-class passagner:
+    def validThis(self, dist):
+        return dist is str
+
+
+class passanger:
     def __init__(self, num):
         self.num = num
-    
+
     def validNumber(self):
         print("this working here")
-        return type(self.num) == int and self.num > 0
+        return self.num is int and self.num > 0
 
     def forHereDiscount(self):
         if 4 < self.num < 10:
             return 0.1
         elif self.num <= 10:
             return 0.2
-        #TODO: add more discount levels if needed
+        # TODO: add more discount levels if needed
         else:
             return 0.0
+
 
 class Plane:
     def __init__(self, dist, num, dur):
@@ -34,7 +36,11 @@ class Plane:
         self.seats = 200
 
     def sum(self):
-        if not self.myclass.validThis(self.dist) or not self.passanger.validNumber() or not self.total_TIME.is_valid_total_TIME():
+        if (
+            not self.myclass.validThis(self.dist) or
+            not self.passanger.validNumber() or
+            not self.total_TIME.is_valid_total_TIME()
+        ):
             return -1
 
         numberTotal = self.costBas
@@ -44,40 +50,46 @@ class Plane:
 
         discount = self.passanger.forHereDiscount()
         numberTotal = numberTotal - (numberTotal * discount)
-        
+
         return max(int(numberTotal), 0)
+
 
 class total_TIME:
     def __init__(self, dur):
         self.dur = dur
 
     def is_valid_total_TIME(self):
-        return type(dur)==int and self.dur > 0
+        return self.dur is int and self.dur > 0
 
     def getFee(self):
         return 200 if self.dur < 7 else 0
 
     def getTheBestPromoEver(self):
         return 200 if self.dur > 30 else 0
-    
+
     def getWeekend(self):
         return 100 if self.dur > 7 else 0
+
 
 class Vacation_:
     costBas = 1000
 
     def __init__(self, dist, num, dur):
         self.myclass = myclass()
-        self.passagner = passagner(num)
+        self.passagner = passanger(num)
         self.total_TIME = total_TIME(dur)
         self.dist = dist
 
     def sum(self):
-        #sum the cost of the vacation package here
-        if not self.myclass.validThis(self.dist) or not self.passagner.validNumber() or not self.total_TIME.is_valid_total_TIME():
+        # sum the cost of the vacation package here
+        if (
+            not self.myclass.validThis(self.dist) or
+            not self.passagner.validNumber() or
+            not self.total_TIME.is_valid_total_TIME()
+        ):
             return -1
-        
-        #sum the total cost
+
+        # sum the total cost
         numberTotal = self.costBas
         numberTotal += self.myclass.get_extraCost(self.dist)
         numberTotal += self.total_TIME.getFee()
@@ -85,27 +97,28 @@ class Vacation_:
 
         discount = self.passagner.forHereDiscount()
         numberTotal = numberTotal - (numberTotal * discount)
-        
+
         return max(int(numberTotal), 0)
 
-#this is main function
+
+# this is main function
 def main():
-    #this are the inputs
+    # this are the inputs
     dist = "Paris"
     num = 5
     dur = 10
-    seats = 400
 
-    #this are the outputs
+    # this are the outputs
     calculator = Vacation_(dist, num, dur)
     cost = calculator.sum()
 
-    #this will do some printing
+    # this will do some printing
     if cost == -1:
         print("Invalid input.")
     else:
         print(f"The total cost of the vacation package is: ${cost}")
 
-#main event function
+
+# main event function
 if __name__ == "__main__":
     main()
